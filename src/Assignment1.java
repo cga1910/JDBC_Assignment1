7ef532884490e8b2fc44d917c82b4fb5f74ad498
gso1910 <gustav.bj@gmail.com>
2019-01-17 13:27:11 +0100

    Kodformatering 2

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -5,24 +5,24 @@ import java.sql.Statement;
 
 public class Assignment1 {
 
-public static Connection getConnection() {
+  public static Connection getConnection() {
 
-  try {
-    String url = "jdbc:mysql://localhost:3306/sqlandjava" +
-                 "?useUnicode=true&useJDBCCompliantTimezoneShift=true" +
-                 "&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    String username = "user";
-    String password = "password";
+    try {
+      String url = "jdbc:mysql://localhost:3306/sqlandjava" +
+                   "?useUnicode=true&useJDBCCompliantTimezoneShift=true" +
+                   "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+      String username = "user";
+      String password = "password";
 
-    Connection conn = DriverManager.getConnection(url,username,password);
-    System.out.println("Connected to database!");
-    return conn;
+      Connection conn = DriverManager.getConnection(url,username,password);
+      System.out.println("Connected to database!");
+      return conn;
 
-  } catch (Exception e) {
-      System.out.println(e);
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    return null;
   }
-  return null;
-}
 
   public static void main(String[] args) throws Exception {
     Connection conn = getConnection();
